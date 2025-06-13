35f1c644a4d63d3b3a8e168cfb2f5901a31a7bf4
Abhaya N K <abhayakhatri48@gmail.com>
2025-06-13 08:40:23 +0545

    created user route

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,16 +1,31 @@
 import { Router } from "express";
 import User from "../models/user.js";
+const userRouter = Router();
 
+userRouter.post('/register', (req, res) => {
+  User.create(req.body)
+  res.send('User registered successfully');
+})
 
-const userRoute = Router();
-userRoute.post("/user", (req, res) => {
-  User.create(req.body);
-  res.send("User created successfully");
-});
-
-userRoute.get("/user", async(req, res) => {
-  const users = await User.find();
+userRouter.get('/register', async(req, res) => {
+  const users = await User.find()
   res.send(users);
-});
+})
 
-export default userRoute;
+
+
+export default userRouter;
+
+
+// const userRoute = Router();
+// userRoute.post("/user", (req, res) => {
+//   User.create(req.body);
+//   res.send("User created successfully");
+// });
+
+// userRoute.get("/user", async(req, res) => {
+//   const users = await User.find();
+//   res.send(users);
+// });
+
+// export default userRoute;
