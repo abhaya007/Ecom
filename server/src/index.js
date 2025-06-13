01ae9016bfe69c7b49ac966c78bf1c66ae7ea1e1
Abhaya N K <abhayakhatri48@gmail.com>
2025-06-13 10:08:42 +0545

    routes for register and user

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import connect from './db/connect.js'
+import cors from 'cors'
 import userRoute from './routes/user.js'
 import userRouter from './routes/user.js'
 const app = express()
 const port = 8000
-connect()
 
+connect()
+app.use(cors())
 app.use(express.json()) 
 app.use(userRouter)
 
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,14 +2,17 @@ import { Router } from "express";
 import User from "../models/user.js";
 const userRouter = Router();
 
-userRouter.post('/register', (req, res) => {
-  User.create(req.body)
-  res.send('User registered successfully');
+userRouter.post('/register', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  if(user)
+     return res.send('Email already exists');
+  else User.create(req.body)
+    return res.send('User registered successfully');
 })
 
-userRouter.get('/register', async(req, res) => {
-  const users = await User.find()
-  res.send(users);
+userRouter.get('/users', async(req, res) => {
+  const data = await User.find()
+  res.send(data);
 })
 
 
