f8f3fad1ad157dcf989ffe50f268d81491ffe9e1
Abhaya N K <abhayakhatri48@gmail.com>
2025-07-05 21:14:45 +0545

    product route

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import connect from './db/connect.js'
 import cors from 'cors'
-import userRoute from './routes/user.js'
 import userRouter from './routes/user.js'
 import dotenv from 'dotenv'
+import productRouter from './routes/product.js'
 dotenv.config()
 
 const app = express()
@@ -13,6 +13,7 @@ connect()
 app.use(cors())
 app.use(express.json()) 
 app.use(userRouter)
+app.use(productRouter)
 
 
 
